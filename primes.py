a2fc09ef61572fe51d9c4c924275ee135378ffdc
YukeshShr <yukeshr@hotmail.com>
2022-11-19 12:19:45 +0000

    Solve primes v2 function and raise ValueError

--- primes.py
+++ primes.py
@@ -3,4 +3,17 @@
 
 def primes(number_of_primes):
     list = []
-    return list
+    count = 2
+    if(number_of_primes <= 0):
+        raise ValueError("Invalid Number! Argument must be greater than 0")
+    else:
+        while(len(list) < number_of_primes):
+
+            for i in range(2, count):
+                if(count % i == 0):
+                    break
+            else:
+                list.append(count)
+            
+            count += 1
+        return list
